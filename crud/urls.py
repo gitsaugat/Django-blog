e7182e14637f2ca9097f239a83b02586b123f59c
= <=>
2020-10-26 14:41:25 +0545

    Create Urls Update Routes Update Views and Add Crud Functionality

--- crud/urls.py
+++ crud/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('' , views.home_view , name = "homepage")
+    path('' , views.home_view , name = "homepage"),
+    path('posts/create/' , views.create_post , name = "create-post"),
+    path('posts/delete/' , views.delete_page , name = "delete-page"),
+    path('post/delete/<int:id>/' , views.deletePost , name = "delete_post"),
+    path('posts/update/' , views.update , name = "update-page"),
+    path('post/update/<int:id>/' , views.update_post , name = "update-post")
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,76 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User 
 # Create your views here.
 
 
 def home_view(request):
-    context = {}
+    context = {
+        'posts' : Post.objects.all(),
+        'title' : 'Homepage'
+    }
     return render(request , "crud/home.html" , context)
+
+
+
+def create_post(request):
+
+    context = {}
+
+    if request.method == "POST":
+
+        title = request.POST["title"]
+        content = request.POST["content"]
+
+        if request.user.is_authenticated:
+            newPost = Post(title = title , content = content , user = request.user)
+            newPost.save()
+
+            return redirect('/')
+        return redirect('/')
+
+    return render(request , "crud/create.html" , context)
+
+
+
+def delete_page(request):
+    context = {'posts' : Post.objects.all()}
+    return render(request , 'crud/delete.html', context)
+
+
+def deletePost(request , id):
+
+    if isinstance(id , int):
+
+        Post.objects.get(id = id).delete()
+
+        return redirect('/')
+
+
+
+def update(request):
+    context = {'posts' : Post.objects.all()}
+    return render(request , 'crud/update_page.html', context)
+
+
+def update_post(request , id):
+
+    
+    context  = {'post'  : Post.objects.get(id = id)}
+
+    if isinstance(id , int):
+
+        if request.method == "POST":
+
+            post = Post.objects.get(id = id)
+
+            post.title = request.POST["title"]
+            post.content = request.POST["content"]
+
+            post.save()
+
+            return redirect('/')
+
+            
+    return render(request , 'crud/update.html' , context)

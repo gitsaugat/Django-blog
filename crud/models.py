7c4c38d6d2bbdc4c4b5c0a8fd256c0f185937546
= <=>
2020-10-26 13:32:45 +0545

    Setup Models And View Templates

--- crud/models.py
+++ crud/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone 
+
+class Post(models.Model):
+
+    content = models.TextField()
+    title   = models.CharField(max_length = 300)
+    user    =  models.ForeignKey(User , on_delete = models.CASCADE)
+    date_published = models.DateTimeField(default = timezone.now())
+
+
+    def __str__(self):
+
+        return self.title 
+
+
+        
+
